98152ca510ef6aef1611bc8db28696661305a2a3
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-03-02 21:54:49 +0530

    hange

--- assets/effi.js
+++ assets/effi.js
@@ -19,90 +19,96 @@ let timeLine = gsap.timeline({
 timeLine.from(".title", {
     y: "-100%",
     opacity:0
-})
+},"-=0.2"
+)
 
     tl.to([".text-one", ".para-one"],
         {
             opacity: 0
-        }
+        },"-=0.2"
     )
     tl.to([".text-two", ".para-two"],
         {
             opacity: 1
-        }
+        },"-=0.2"
     )
     tl.to([".text-two", ".para-two"],
         {
             opacity: 0
-        }
+        },"-=0.2"
     )
     tl.to([".text-three", ".para-three"],
         {
             opacity: 1
-        }
+        },"-=0.2"
     )
     tl.to([".text-three", ".para-three"],
         {
             opacity: 0
-        }
+        },"-=0.2"
     )
     tl.to([".text-four", ".para-four"],
         {
             opacity: 1
-        }
+        },"-=0.2"
     )
     tl.to([".text-four", ".para-four"],
         {
             opacity: 0
-        }
+        },"-=0.2"
     )
     tl.to([".text-five", ".para-five"],
         {
             opacity: 1
-        }
+        },"-=0.2"
     )
     tl.to([".text-five", ".para-five"],
         {
             opacity: 0
-        }
+        },"-=0.2"
     )
     tl.to([".text-six", ".para-six"],
         {
             opacity: 1
-        }
+        },"-=0.2"
+    )
+    tl.to([".text-six", ".para-six"],
+        {
+            opacity: 0
+        },"-=0.2"
     )
     tl.to(".card-one",
         {
             opacity: 0
-        }
+        },"-=0.2"
     )
     tl.to(".img-one",
         {
             opacity: 0
-        }
+        },"-=0.2"
     )
     tl.to([".card-two",".img-two"],
         {
             opacity: 1
-        }
+        },"-=0.2"
     )
     tl.to([".card-two", ".img-two"],
         {
             opacity: 0
-        }
+        },"-=0.2"
     )
     tl.to([".card-three",".img-one"],
         {
             opacity: 1
-        }
+        },"-=0.2"
     )
     tl.to([".card-three",".img-one"],
         {
             opacity: 0
-        }
+        },"-=0.2"
     )
     tl.to([".card-four",".img-two"],
         {
             opacity: 1
-        }
+        },"-=0.2"
     )
